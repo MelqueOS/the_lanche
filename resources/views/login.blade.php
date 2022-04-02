c424d1e685d9d2263917fc11c30204b69d04bc0e
tharlisonS2 <tharlisonptc2@gmail.com>
2022-04-02 17:41:09 -0300

    Comit login4

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,6 +1,6 @@
 
 
-@extends("templates.templatelogin")
+@extends("templates.template")
 @section("titulo", "Login")
 @section('login')
 
@@ -10,10 +10,12 @@
     <div class="caixal">
         <form class="formlogin">
             <img class="logologin"src="imagens/Group47.svg">
+        <div class="botoes">
         <div><button class="btngoogle">ENTRAR COM GOOGLE</button></div>
         <div><button class="btnemail">ENTRAR COM EMAIL</button></div>
         <div class="textoentrar">
             <a >ENTRAR COMO CONVIDADO</a>
+        </div>
         </div>
         <div class="textocadastro">
             <a href="#">CADASTRO</a>
