48388555b441bcefb5adec50c7c785689b33fee3
tharlisonS2 <tharlisonptc2@gmail.com>
2022-04-02 16:57:54 -0300

    Comit login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,27 @@
+
+
+@extends("templates.templatelogin")
+@section("titulo", "Login")
+@section('login')
+
+    
+    <div class="cor">
+        <div class="linha"></div>
+    <div class="caixal">
+        <form class="formlogin">
+            <img class="logologin"src="imagens/Group47.svg">
+        <div><button class="btngoogle">ENTRAR COM GOOGLE</button></div>
+        <div><button class="btnemail">ENTRAR COM EMAIL</button></div>
+        <div class="textoentrar">
+            <a >ENTRAR COMO CONVIDADO</a>
+        </div>
+        <div class="textocadastro">
+            <a href="#">CADASTRO</a>
+        </div>
+    </div>
+</form>
+    <div class="caixaq">
+
+    </div>
+</div>
+@endsection
---
+++ resources/views/templates/templatelogin.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<head>
+<title> @yield("titulo")</title>
+   <link rel="stylesheet"href="{{asset('css/bootstrap.css')}}"/>
+   <link rel="stylesheet"href="{{asset('css/bootstrap-icons.css')}}"/>
+   <link rel="stylesheet"href="{{asset('css/custom.css')}}"/>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    
+@yield("login")
+    
+</body>
+</html>
