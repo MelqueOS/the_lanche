44667b28a1babf0d858dbec6d3b5b6c763c4af52
tharlisonS2 <tharlisonptc2@gmail.com>
2022-04-02 17:48:03 -0300

    Comit login5

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -11,11 +11,15 @@
         <form class="formlogin">
             <img class="logologin"src="imagens/Group47.svg">
         <div class="botoes">
-        <div><button class="btngoogle">ENTRAR COM GOOGLE</button></div>
-        <div><button class="btnemail">ENTRAR COM EMAIL</button></div>
-        <div class="textoentrar">
-            <a >ENTRAR COMO CONVIDADO</a>
-        </div>
+            <div class="">
+                <button class="btngoogle">ENTRAR COM GOOGLE</button>
+            </div>
+            <div class="">
+                <button class="btnemail">ENTRAR COM EMAIL</button>
+            </div>
+            <div class="textoentrar">
+                <a >ENTRAR COMO CONVIDADO</a>
+            </div>
         </div>
         <div class="textocadastro">
             <a href="#">CADASTRO</a>
