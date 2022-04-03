babb3a45c5913f138557e3b0550b632fd16ef3cf
Pablo <pabloph0077@gmail.com>
2022-04-03 00:09:37 -0300

    finalizacao da tela de login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,32 +1,36 @@
-
-
 @extends("templates.template")
 @section("titulo", "Login")
+
+
 @section('login')
+<div class="box-scan">
+    <div class="scan">
+        <button class='botao'>
+            <i class="bi bi-qr-code-scan"></i><br> LER QRCODE
+        </button>
 
-    
-    <div class="caixalogin">
-        <div class="linha"></div>
-    <div class="caixal">
-    <img class="logologin"src="img/logo.svg">
-        <form class="formlogin">
-            
-        <div class="botoes">
-            <div class="">
-                <button class="btngoogle">ENTRAR COM GOOGLE</button>
-            </div>
-            <div class="">
-                <button class="btnemail">ENTRAR COM EMAIL</button>
-            </div>
-            <div class="textoentrar">
-                <a >ENTRAR COMO CONVIDADO</a>
-            </div>
-        </div>
-        <div class="textocadastro">
-            <a href="#">Cadastrar</a>
-        </div>
     </div>
-</form>
+</div>
 
+<div class="box-formulario">
+    <form action="">
+        <!-- TEXT -->
+        <div class='text-login'>
+            <p>Login</p>
+        </div>
+
+        <div class="login">
+            <label for="">Usuario</label>
+            <input type="email" name="" id="">
+        </div>
+
+        <div class="passwd">
+            <label for="">Senha</label>
+            <input type="password" name="" id="">
+        </div>
+
+        <div class="btn">
+            <button class='btn-confirm'>Entrar</button></div>
+    </form>
 </div>
 @endsection
--- resources/views/templates/template.blade.php
+++ resources/views/templates/template.blade.php
@@ -1,19 +1,28 @@
 <!DOCTYPE html>
+
 <head>
-<title> @yield("titulo")</title>
-   <link rel="stylesheet"href="{{asset('css/bootstrap.css')}}"/>
-   <link rel="stylesheet"href="{{asset('css/bootstrap-icons.css')}}"/>
-   <link rel="stylesheet"href="{{asset('css/custom.css')}}"/>
-   <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;800&display=swap" rel="stylesheet">
    <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title> @yield("titulo")</title>
+
+   <!-- BOOTSTRAP STYLE -->
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+   <!-- BOOTSTRAP ICONS -->
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+
+   <!-- CSS -->
+   <link rel="stylesheet" href="{{asset('css/custom.css')}}" />
+   
+   <!-- GOOGLE FONTS -->
+   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;800&display=swap" rel="stylesheet">
+
 </head>
+
 <body>
-<div class="">  
-@yield("login")
-</div>
+    <div class="container">
+        @yield("login")
+    </div>
 </body>
+
 </html>
