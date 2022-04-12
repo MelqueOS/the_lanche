758a80e22197ad2b08314917194f1a502f52785e
Pablo <pabloph0077@gmail.com>
2022-04-12 20:26:59 -0300

    ideia de validacao(prof jean)

--- public/js/function.js
+++ public/js/function.js
@@ -1,4 +1,4 @@
-$(function () {
+$(function() {
     var atual_fs, next_fs, prev_fs;
     var formulario = $('form[name=formulario]');
 
@@ -11,7 +11,7 @@ $(function () {
         next_fs.show(800);
     }
 
-    $('.prev').click(function () {
+    $('.prev').click(function() {
         atual_fs = $(this).parent();
         prev_fs = $(this).parent().prev();
 
@@ -20,7 +20,7 @@ $(function () {
         prev_fs.show(800);
     })
 
-    $('input[name=next1]').click(function () {
+    $('input[name=next1]').click(function() {
         var array = formulario.serializeArray();
         if (array[0].value == '' || array[1].value == '' || array[2].value == '' || array[3].value == '') {
             $('.resp').html('<div class="erros"><p>Preencha todos os dados da primeira etapa!</p></div>');
@@ -31,7 +31,7 @@ $(function () {
     })
 
 
-    $('input[name=next2]').click(function () {
+    $('input[name=next2]').click(function() {
         var array = formulario.serializeArray();
         if (array[4].value == '' || array[5].value == '' || array[6].value == '') {
             $('.resp').html('<div class="erros"><p>Preencha todos os dados sobre os seus contatos!</p></div>');
@@ -41,7 +41,7 @@ $(function () {
         }
     });
 
-    $('input[type=submit').click(function (evento) {
+    $('input[type=submit').click(function(evento) {
         evento.preventDefault();
         var array = formulario.serializeArray();
         if (array[8].value == '' || array[9].value == '') {
@@ -55,7 +55,25 @@ $(function () {
 
 });
 
-$(document).ready(function () {
+$(document).ready(function() {
     $('.cnpj').mask('99.999.999/9999-99');
     $('.phone').mask('(99) 9999-9999');
 })
+
+
+
+
+
+// valido = true
+// $('.form-control:visible').each(function() {
+//     if ($(this).val() == '') {
+//         $(this).addClass('is-invalid');
+//         $(this).next('.invalid-feedback').show();
+//         valido = false;
+//     } else {
+//         $(this).removeClass('is-invalid');
+//         $(this).next('.invalid-feedback').hide();
+//     }
+// });
+
+// if (valido)
