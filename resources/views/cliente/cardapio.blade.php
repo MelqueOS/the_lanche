4d9a3ccf7892a34aeca9fd535319cb30fb744eb6
Pablo <pabloph0077@gmail.com>
2022-04-17 22:52:39 -0300

    comentando as linhas para o melque preencher

--- resources/views/cliente/cardapio.blade.php
+++ resources/views/cliente/cardapio.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Nome da empresa aqui -->
     <title>Cardapio da Empresa X</title>
 
     <!-- CSS -->
@@ -14,6 +15,7 @@
 </head>
 
 <body>
+    <!-- NAVBAR -->
     <div class="navbar navbar-expand-lg col-12">
         <div class="cont-form ">
             <form action="" method="get" class='form-group'>
@@ -22,50 +24,137 @@
         </div>
     </div>
 
-    <div class="itens">
+    <!-- AQUI VAI TER UM FOREACH >> AQUI VAI SER O TIPO(COMBO, COMIDA, BEBIDA, ETC)-->
+    <div class="tipo">
+        <!-- descricao do tipo -->
         <div class='title'>
             <h1>Combos</h1>
         </div>
 
-        <div class="box-item">
-            <div class='box-img'>
-                <img src="{{asset('img/logo.svg')}}" alt="">
+        <div class="conteudo">
+
+            <!-- ITEM 1 -->
+            <div class="item">
+                <!-- AQUI VAI TER UM FOREACH >> MOSTRA TODOS OS ITENS DO TIPO -->
+                <div class="box-item">
+                    <div class='box-img'>
+                        <!-- MOSTRA A FOTO DO ITEM -->
+                        <img src="{{asset('img/logo.svg')}}" alt="">
+                    </div>
+
+                    <div class="sub-box">
+                        <div class="sub-title">
+                            <!-- nome_descritivo -->
+                            <h2>Prato do Mar</h2>
+                        </div>
+                        <div class='descricao'>
+                            <!-- descricao -->
+                            <p>Prato "viagem ao fundo do mar" poderia ser descrita como "polvo cozido, lula Prato "viagem ao fundo do mar" poderia em ser descrita como "polvo cozido, lula em anéis e camarões refogados servidos com anéis e camarões refogados servidos com caldo de peixe especial do chefe".</p>
+                        </div>
+
+                        <hr>
+
+                        <div class='valor'>
+                            <!-- valor do item -->
+                            <p><strong>Valor: </strong>R$25,00</p>
+                        </div>
+                    </div>
+                </div>
             </div>
 
-            <div class="sub-box">
-                <div class="sub-title">
-                    <h2>Prato do Mar</h2>
+            <!-- ITEM 2 -->
+            <div class="item">
+                <!-- AQUI VAI TER UM FOREACH >> MOSTRA TODOS OS ITENS DO TIPO -->
+                <div class="box-item">
+                    <div class='box-img'>
+                        <!-- MOSTRA A FOTO DO ITEM -->
+                        <img src="{{asset('img/logo.svg')}}" alt="">
+                    </div>
+
+                    <div class="sub-box">
+                        <div class="sub-title">
+                            <!-- nome_descritivo -->
+                            <h2>Prato do Mar</h2>
+                        </div>
+                        <div class='descricao'>
+                            <!-- descricao -->
+                            <p>Prato "viagem ao fundo do mar" poderia ser descrita como "polvo cozido, lula Prato "viagem ao fundo do mar" poderia em ser descrita como "polvo cozido, lula em anéis e camarões refogados servidos com anéis e camarões refogados servidos com caldo de peixe especial do chefe".</p>
+                        </div>
+
+                        <hr>
+
+                        <div class='valor'>
+                            <!-- valor do item -->
+                            <p><strong>Valor: </strong>R$25,00</p>
+                        </div>
+                    </div>
                 </div>
-                <div class='descricao'>
-                    <p>Prato "viagem ao fundo do mar" poderia ser descrita como "polvo cozido, lula Prato "viagem ao fundo do mar" poderia em ser descrita como "polvo cozido, lula em anéis e camarões refogados servidos com anéis e camarões refogados servidos com caldo de peixe especial do chefe".</p>
+            </div>
+
+            <!-- ITEM 3 -->
+            <div class="item">
+                <!-- AQUI VAI TER UM FOREACH >> MOSTRA TODOS OS ITENS DO TIPO -->
+                <div class="box-item">
+                    <div class='box-img'>
+                        <!-- MOSTRA A FOTO DO ITEM -->
+                        <img src="{{asset('img/logo.svg')}}" alt="">
+                    </div>
+
+                    <div class="sub-box">
+                        <div class="sub-title">
+                            <!-- nome_descritivo -->
+                            <h2>Prato do Mar</h2>
+                        </div>
+                        <div class='descricao'>
+                            <!-- descricao -->
+                            <p>Prato "viagem ao fundo do mar" poderia ser descrita como "polvo cozido, lula Prato "viagem ao fundo do mar" poderia em ser descrita como "polvo cozido, lula em anéis e camarões refogados servidos com anéis e camarões refogados servidos com caldo de peixe especial do chefe".</p>
+                        </div>
+
+                        <hr>
+
+                        <div class='valor'>
+                            <!-- valor do item -->
+                            <p><strong>Valor: </strong>R$25,00</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-
-        <hr>
-
     </div>
 
-    <div class="itens">
-        <div class='title'>
-            <h1>Combos</h1>
-        </div>
+    <hr>
 
-        <div class="box-item">
-            <div class='box-img'>
-                <img src="{{asset('img/logo.svg')}}" alt="">
+    <!-- DESCONSIDERAR! DAQUI PARA BAIXO É SOMENTE PARA EXEMPLIFICAR MESMO -->
+    <div class="tipo">
+        <div class="item">
+            <!-- descricao do tipo -->
+            <div class='title'>
+                <h1>Pastel</h1>
             </div>
-
-            <div class="sub-box">
-                <div class="sub-title">
-                    <h2>Frutos do mar</h2>
+            <!-- AQUI VAI TER UM FOREACH >> MOSTRA TODOS OS ITENS DO TIPO -->
+            <div class="box-item">
+                <div class='box-img'>
+                    <!-- MOSTRA A FOTO DO ITEM -->
+                    <img src="{{asset('img/logo.svg')}}" alt="">
                 </div>
-                <div class='descricao'>
-                    <p>Prato "viagem ao fundo do mar" poderia ser descrita como "polvo cozido, lula Prato "viagem ao fundo do mar" poderia em ser descrita como "polvo cozido, lula em anéis e camarões refogados servidos com anéis e camarões refogados servidos com caldo de peixe especial do chefe".</p>
+                <div class="sub-box">
+                    <div class="sub-title">
+                        <!-- nome_descritivo -->
+                        <h2>Pastel de Queijo</h2>
+                    </div>
+                    <div class='descricao'>
+                        <!-- descricao -->
+                        <p>alimento composto por uma massa à base de farinha a que se dá a forma de um envelope, se recheia e depois se frita por imersão em óleo fervente.</p>
+                    </div>
+                    <hr>
+                    <div class='valor'>
+                        <!-- valor do item -->
+                        <p><strong>Valor: </strong>R$25,00</p>
+                    </div>
                 </div>
             </div>
+            <hr>
         </div>
-
     </div>
 </body>
 
