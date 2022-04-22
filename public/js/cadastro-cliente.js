54da61ff6946cfc96c1294d54a098eb0156d5ea2
Pablo <pabloph0077@gmail.com>
2022-04-21 22:24:02 -0300

    validacao dos campos

---
+++ public/js/cadastro-cliente.js
@@ -0,0 +1,76 @@
+$(function () {
+    var atual_fs, next_fs, prev_fs;
+    var formulario = $('form[name=formulario]');
+
+    function next(elem) {
+        atual_fs = $(elem).parent();
+        next_fs = $(elem).parent().next();
+
+        $('#progressbar li').eq($('fieldset').index(next_fs)).addClass('active');
+        atual_fs.hide(800);
+        next_fs.show(800);
+    }
+
+    $('.prev').click(function () {
+        atual_fs = $(this).parent();
+        prev_fs = $(this).parent().prev();
+
+        $('#progressbar li').eq($('fieldset').index(atual_fs)).removeClass('active');
+        atual_fs.hide(800);
+        prev_fs.show(800);
+    })
+
+    // validacao dos campos em branco
+    
+    $('button[name=next1]').click(function () {
+        var array = formulario.serializeArray();
+        if (array[1].value == '' || array[2].value == '' || array[3].value == '' || array[4].value == '' || array[5].value == '') {
+            $('.resp').html('<div class="erros"><p>Preencha todos os dados da primeira etapa!</p></div>');
+        } else {
+            $('.resp').html('');
+            next($(this));
+        }
+    })
+
+
+    $('button[name=next2]').click(function () {
+        var array = formulario.serializeArray();
+        if (array[6].value == '' || array[7].value == '') {
+            $('.resp').html('<div class="erros"><p>Preencha todos os dados sobre os seus contatos!</p></div>');
+        } else {
+            $('.resp').html('');
+            next($(this));
+        }
+    });
+
+    $('button[type=submit]').click(function (evento) {
+        evento.preventDefault();
+        var array = formulario.serializeArray();
+        if (array[9].value == '' || array[10].value == '') {
+            $('.resp').html('<div class="erros"><p>Preencha todos os dados sobre seu endereço</p></div>');
+        } else {
+            $('.resp').html('<div class="ok"><p>Sua requisição foi enviada!</p></div>');
+            next($(this));
+        }
+
+    });
+
+});
+
+
+
+
+
+// valido = true
+// $('.form-control:visible').each(function() {
+//     if ($(this).val() == '') {
+//         $(this).addClass('is-invalid');
+//         $(this).next('.invalid-feedback').show();
+//         valido = false;
+//     } else {
+//         $(this).removeClass('is-invalid');
+//         $(this).next('.invalid-feedback').hide();
+//     }
+// });
+
+// if (valido)
--- resources/views/cliente/cadastro_edit.blade.php
+++ resources/views/cliente/cadastro_edit.blade.php
@@ -26,39 +26,39 @@
 
         <fieldset class="">
 
-            <label class='form-control' for="">Nome</label>
+            <label for="">Nome</label>
             <input class='form-control' type="text" name="nome" required />
 
-            <label class='form-control' for="">Email</label>
+            <label for="">Email</label>
             <input class='form-control' type="email" name="email" required />
 
-            <label class='form-control' for="">Data de Nascimento</label>
+            <label for="">Data de Nascimento</label>
             <input class='form-control' type="date" name="datanascimento" required min='1900-01-01' />
 
-            <label class='form-control' for="">Senha</label>
+            <label for="">Senha</label>
             <input class='form-control' type="password" name="pass" required />
             
-            <label class='form-control' for="">Confirmar Senha</label>
+            <label for="">Confirmar Senha</label>
             <input class='form-control' type="password" name="csenha" required />
 
             <button type="button" name="next1" class="next action-button btn-primary">Proximo</button>
 
-            <label class='form-control' class='nv-cadastro'>Já possui uma conta? <a href="/">Entre agora</a></label>
+            <label class='nv-cadastro'>Já possui uma conta? <a href="/">Entre agora</a></label>
         </fieldset>
 
         <fieldset class="">
-            <label class='form-control' for="">Telefone</label>
-            <input class='form-control' type="text" name="telefone" class='phone' maxlength='15' required>
-            <label class='form-control' for="">WhatsApp</label>
-            <input class='form-control' type="text" name="whatsapp" class='phone' maxlength='15' required>
+            <label for="">Telefone</label>
+            <input class='form-control phone' type="text" name="telefone" maxlength='15' required>
+            <label for="">WhatsApp</label>
+            <input class='form-control phone' type="text" name="whatsapp"  maxlength='15' required>
             <button type="button" name="previous" class="prev action-button btn-second">Anterior</button>
             <button type="button" name="next2" class="next action-button btn-primary">Proximo</button>
 
-            <label class='form-control' class='nv-cadastro'>Já possui uma conta? <a href="/">Entre agora</a></label>
+            <label class='nv-cadastro'>Já possui uma conta? <a href="/">Entre agora</a></label>
         </fieldset>
 
         <fieldset class="">
-            <label class='form-control' for="">Tipo Logradouro</label>
+            <label for="">Tipo Logradouro</label>
             <select name="tipologadouro" class="inptselect" required>
                 <option value="1" selected="selected">Avenida</option>
                 <option value="2">Rua</option>
@@ -67,25 +67,26 @@
                 <option value="5">Condominio</option>
             </select>
 
-            <label class='form-control' for="">bairro</label>
+            <label for="">bairro</label>
             <input class='form-control' type="text" name="logradouro" required>
-            <label class='form-control' for="">numero</label>
+            <label for="">numero</label>
             <input class='form-control' type="number" name="numero" required>
-            <label class='form-control' for="">referencia</label>
+            <label for="">referencia</label>
             <input class='form-control' type="text" name="referencia">
-            <label class='form-control' for="">complemento</label>
-            <input type="text" name="complemento">
-            <input type="button" name="previous" class="prev action-button btn-second" value="Anterior" />
-            <button type="submit" class="submit action-button btn-primary">Finalizar</button>
+            <label for="">complemento</label>
+            <input class='form-control' type="text" name="complemento">
+            <button type="button" name="previous" class="prev action-button btn-second">Anterior</button>
+            <button type="submit" name="next" class="submit action-button btn-primary">Finalizar</button>
 
-            <label class='form-control' class='nv-cadastro'>Já possui uma conta? <a href="/">Entre agora</a></label>
+            <label class='nv-cadastro'>Já possui uma conta? <a href="/">Entre agora</a></label>
 
         </fieldset>
     </form>
     </div>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script type='text/javascript' src='js/function.js'></script>
+    <script type='text/javascript' src='js/default.js'></script>
+    <script type='text/javascript' src='js/cadastro-cliente.js'></script>
 
     <!-- JQ MASK -->
     <script src="{{asset('js/jquery.mask.js')}}"></script>
