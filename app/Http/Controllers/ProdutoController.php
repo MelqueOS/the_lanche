75be86f839e6a970efc6818dca52de2741100dc3
tharlisonS2 <tharlisonptc2@gmail.com>
2022-06-05 15:45:45 -0300

    rotas

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -15,9 +15,7 @@ class ProdutoController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function __construct(){
-        $this->middleware("auth");       
-    }
+
     public function index()
     {
         
