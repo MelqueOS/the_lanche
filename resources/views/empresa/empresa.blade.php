8cb326c8f247a1858245e2487cb7c451eadb9214
Pablo <pabloph0077@gmail.com>
2022-06-06 12:02:23 -0300

    alteracao das telas e add a navbar

--- resources/views/empresa/empresa.blade.php
+++ resources/views/empresa/empresa.blade.php
@@ -1,4 +1,5 @@
 <html>
+
 <head>
     <title>{{$titulo}} {{$user->name}}</title>
 
@@ -6,46 +7,49 @@
     <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}" />
     <link rel="stylesheet" href="{{asset('css/bootstrap-icons.css')}}" />
     <link rel='stylesheet' href="{{asset('css/padroes.css')}}" />
-    <link rel="stylesheet" href="{{asset('css/login.css')}}" />
     <link rel="stylesheet" href="{{asset('css/nav.css')}}" />
-    
+    <link rel="stylesheet" href="{{asset('css/empresa.css')}}">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
 
 </head>
+
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark navedit ">
         <div class="container-fluid">
-            <a class="navbar-brand" href="/">Thelanche</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                    <a class="nav-link active" aria-current="page" href="/produto">Gerenciar cardapio</a>
-                    {{-- Sera adicionado no futuro
-                    <a class="nav-link" href="/pedido">Gerenciar Pedidos</a>
-                    <a class="nav-link" href="/mesa">Adicionar Mesa</a>
+            <div class="inicial">
+                <a class="navbar-brand" href="/">The Lanche</a>
+                <a class="navbar-brand" aria-current="page" href="/produto">Gerenciar cardapio</a>
+                <!-- futuro -->
+                {{-- Sera adicionado no futuro
+                    <a class="navbar-brand" href="/pedido">Gerenciar Pedidos</a>
+                    <a class="navbar-brand" href="/mesa">Adicionar Mesa</a>
                     --}}
-                    <a class="nav-link" href="/sair">Sair</a>
-                </div>
+                <a class="navbar-brand" href="/sair">Sair</a>
             </div>
+            <div class="final"><a class="navbar-brand" href=''>{{$user->name}}</a></div>
         </div>
     </nav>
-    <div class="d-flex flex-column align-items-center h-75 w-100">
-        <p class="h4 m-2">Bem vindo: {{$user->name}}, {{$acess}}!</p>
-        <table class="table w-75 m-5">
+    <!--Menu Bar -->
+
+    <header>
+        <!-- <h1 class="h4 m-2">Bem vindo: {{$user->name}}, {{$acess}}!</h1> -->
+        <h1 id="titulo">painel de gerenciamento</h1>
+    </header> <!-- HEADER -->
+
+    <main class="d-flex flex-column align-items-center h-75 w-100">
+        <table class="table table-hover w-75 m-3">
+            <colgroup>
+                <col width="10">
+                <col width="100">
+                <col width="200">
+                <col width="200">
+                <col width="200">
+                <col width="250">
+                <col width="250">
+            </colgroup>
+
             <thead>
-                <colgroup>
-                    <col width="10">
-                    <col width="100">
-                    <col width="200">
-                    <col width="200">
-                    <col width="200">
-                    <col width="250">
-                    <col width="250">
-                </colgroup>
                 <tr>
                     <th scope="col">Data de ingresso no sistema </th>
                     <th scope="col">Endereco cadastrado </th>
@@ -57,18 +61,19 @@
                     <th scope="col">Ações</th>
                 </tr>
             </thead>
+
             <tbody>
                 <tr>
                     <td>{{ \Carbon\Carbon::parse($user->created_at)->format('d/m/Y')}}</td>
                     <td>{{$tipo[$endereco->tipo_logradouro]}} {{$endereco->logradouro}}, n {{$endereco->numero}}, bairro {{$endereco->bairro}}</td>
                     <td>{{$user->email}}</td>
                     <td>{{$empresa->telefone}}</td>
-                    
+
 
                     <td>{{ \Carbon\Carbon::parse($user->updated_at)->format('d/m/Y')}} as {{ \Carbon\Carbon::parse($user->updated_at)->format('H:m')}}</td>
                     <td>{{ \Carbon\Carbon::parse($empresa->updated_at)->format('d/m/Y')}}</td>
                     <td>{{ \Carbon\Carbon::parse($endereco->updated_at)->format('d/m/Y')}} </td>
-                    <td>  
+                    <td>
                         <div class="d-flex">
                             {{-- Por enquanto a empresa não pode excluir a conta
                             
@@ -78,27 +83,31 @@
                             <button type='submit' class="btn btn-danger"><i class="bi bi-trash"></i></button>
                             </form>
                             --}}
-                            <a href = "/empresa/{{$user->id}}/edit"><button type='button' class="btn btn-info"><i class="bi bi-pencil-square"></i></button></a>
+                            <a href="/empresa/{{$user->id}}/edit"><button type='button' class="btn btn-info"><i class="bi bi-pencil-square"></i></button></a>
                         </div>
-                    </td>    
+                    </td>
                 </tr>
             </tbody>
         </table>
-    </div>
-            {{-- Codigo para exclusão dos dados da empresa
+    </main> <!-- MAIN -->
+
+
+    {{-- Codigo para exclusão dos dados da empresa
                 <form method = "POST" action = "/empresa/{{$user->id}}">
-                @csrf
-                <input type = "hidden" name = "_method" value = "DELETE"/> 
-	
-                @if($confirmed == true)
-                    <p>Uma solicitação de excluzão da sua conta foi recebida, tem certeza que deseja exclui-la?<p>
-    				<input type = "hidden" name = "confirmed" value = "{{True}}"/> 
-                    <input type = "submit" id = "excluirBotao" name="confirmed" class="btn btn-danger" value = "Cancelar"/>
-                @else
-                    <input type = "hidden" name = "confirmed" value = "{{False}}"/> 
-                @endif
-                   <input type = "submit" id = "excluirBotao" class="btn btn-danger" value = "Excluir conta"/>
-            </form>
-            --}}
-    </body>
+    @csrf
+    <input type="hidden" name="_method" value="DELETE" />
+
+    @if($confirmed == true)
+    <p>Uma solicitação de excluzão da sua conta foi recebida, tem certeza que deseja exclui-la?
+    <p>
+        <input type="hidden" name="confirmed" value="{{True}}" />
+        <input type="submit" id="excluirBotao" name="confirmed" class="btn btn-danger" value="Cancelar" />
+        @else
+        <input type="hidden" name="confirmed" value="{{False}}" />
+        @endif
+        <input type="submit" id="excluirBotao" class="btn btn-danger" value="Excluir conta" />
+        </form>
+        --}}
+</body>
+
 </html>
--- resources/views/produtos/painel_ger_edit.blade.php
+++ resources/views/produtos/painel_ger_edit.blade.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" href="{{asset('css/bootstrap-icons.css')}}" />
     <link rel="stylesheet" href="{{asset('css/padroes.css')}}">
     <link rel="stylesheet" href="{{asset('css/painel_gerenciamento.css')}}" />
+    <link rel="stylesheet" href="{{asset('css/nav.css')}}">
     <!-- JQUERY -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 
@@ -13,184 +14,182 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js" integrity="sha512-pHVGpX7F/27yZ0ISY+VVjyULApbDlD0/X0rgGbTqCE7WFW5MezNTWG/dnhtbBuICzsd0WQPgpE4REBLv+UqChw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </head>
 
-<div class="conteiner-fluid">
-    <div class="d-flex flex-column">
-        <header class="painel d-flex flex-column align-items-center">
-            <h1>{{$secao[1]}}</h1>
-            <figure class="max-width col">
-                @if($produto->url_img != null)
-                <img alt="" id="imgHead" src="{{asset($produto->url_img);}}" class="imgPhoto">
-                @else
-                <img alt="" id="imgPhoto" src="{{asset('img/mais.webp')}}" class="imgPhoto">
-                @endif
-            </figure>
-        </header>
-        <main class="content">
-            <div class='cadastro'>
-                <form action="/produto" method="POST" enctype="multipart/form-data" onsubmit="carregaDados()" class="row formulario">
-                    @csrf
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark navedit ">
+        <a class="navbar-brand" href="/produto">The Lanche</a>
+        <a class="navbar-brand" aria-current="page" href="/empresa">Dados da empresa</a>
+        <!-- futuro -->
+        {{-- Sera adicionado no futuro
+                <a class="navbar-brand" href="/pedido">Gerenciar Pedidos</a>
+                <a class="navbar-brand" href="/mesa">Adicionar Mesa</a>
+                --}}
+        <a class="navbar-brand" href="/sair">Sair</a>
+    </nav>
+    <!--Menu Bar -->
 
-                    <div class="file form-group">
-                        <label for="">Imagem Do Produto</label>
-                        @if($img_lock == "disable")
-                        <input type="file" class='form-control col-11' id="flImage" name="imagem_produto" accept="image/png, image/jpeg" required />
-                        @else
-                        <input type="file" class='form-control' id="flImage" name="imagem_produto" accept="image/png, image/jpeg" />
-                        <input type="hidden" name="att_url" value="{{$produto->url_img}}" />
-                        @endif
-                    </div>
-
-                    <div class="line-two">
-                        <div class="form-group content-produto col-5">
-                            <label for="nome_descritivo">Nome do Produto </label>
-                            <input type="text" name="nome_descritivo" class="form-control" value="{{$produto->nome_descritivo}}" required />
-                        </div>
-                        <div class="form-group content-tipo col-4">
-                            <label for="tipo">Tipo de produto</label>
-                            <select name="tipo" class="form-control" class="inptselect form-control" required>
-                                @foreach($parametro_select as $key_selected => $value_selected)
-                                <option value="{{$key_selected}}">{{$value_selected}}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                        <div class="form-group content-preco col-2">
-                            <label for="valor">Preço</label>
-                            <div id='preco' class="input-group ">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text" id="basic-addon1">R$</span>
-                                </div>
-                                <input type="text" id="valor" maxlength="8" name="valor" class="form-control" value="@if($produto->valor != 0)
-                                    {{number_format($produto->valor, 2, ',', '.')}}
-                                    @endif" />
-                            </div>
-                            <input type="hidden" id="valor_without_mask" name="valor_without_mask" value="{{ $produto->valor }}" />
-                        </div>
-                    </div>
-
-                    <div class="form-group col-12 descricao-form">
-                        <label for="descricao">Descriçao</label>
-                        <textarea class="textarea form-control" rows="2" name="descricao">{{$produto->descricao}}</textarea>
-                    </div>
-                    <div class="button-form row">
-                        <div class="col-2">
-                            <a href='/produto' type="button" class="btn btn-second bottom "><i class="fas fa-save"></i>Cancelar</a>
-                        </div>
-                        <div class="col-2">
-                            <button type="submit" class="btn btn-primary bottom"><i class="fas fa-save"></i>Salvar</button>
-                        </div>
-                    </div>
-                    <input type="hidden" name="id" value="{{$produto->id}}">
-                    <input type="hidden" name="tokid" value="{{$tokid}}">
-                </form>
-            </div>
-        </main>
-    </div>
-    <hr>
-    <div class="produtos-genericos  ">
-        <div class="content">
-            <!--contagem-->
-            <div class="count">
-                <h2>{{$secao[2]}}</h2>
-                <p class='lead'>total de produtos cadastrados: {{count($produtos)}}</p>
-            </div>
-
-            <!-- listagem -->
-            <div class="listagem">
-                @php
-                $conferir=[];
-                @endphp
-
-                @foreach($produtos as $linha)
-
-                @if(in_array($linha->tipo, $conferir) == true)
-
-                @else
-                <hr>
-                <div class='item'>
-                    <div class="tipo d-flex flex-column">
-                        <h1>
-                            @foreach($parametro_select as $key_selected => $value_selected)
-                            @if($linha->tipo == $key_selected)
-                            {{$value_selected}}
-                            {{$conferir[]=$linha->tipo}}
+    <div class="mx-5">
+        <div class="d-flex flex-column ">
+            <header class="painel d-flex flex-column align-items-center">
+                <h1 id='titulo'>{{$secao[1]}}</h1>
+                <figure class="max-width col">
+                    @if($produto->url_img != null)
+                    <img alt="" id="imgHead" src="{{asset($produto->url_img);}}" class="imgPhoto">
+                    @else
+                    <img alt="" id="imgPhoto" src="{{asset('img/mais.webp')}}" class="imgPhoto">
+                    @endif
+                </figure>
+            </header>
+            <main class="content">
+                <div class='cadastro'>
+                    <form action="/produto" method="POST" enctype="multipart/form-data" onsubmit="carregaDados()" class="row formulario">
+                        @csrf
+                        <div class="file form-group">
+                            <label for="">Imagem Do Produto</label>
+                            @if($img_lock == "disable")
+                            <input type="file" class='form-control col-11' id="flImage" name="imagem_produto" accept="image/png, image/jpeg" required />
+                            @else
+                            <input type="file" class='form-control' id="flImage" name="imagem_produto" accept="image/png, image/jpeg" />
+                            <input type="hidden" name="att_url" value="{{$produto->url_img}}" />
                             @endif
-                            @endforeach
-                        </h1>
-                    </div>
-                    <div class="d-flex flex-row flex-wrap aqui">
-
-                        @foreach($produtos as $linha2)
-                        @if($linha->tipo==$linha2->tipo)
-
-                        <div class="cont me-5 mb-5 shadow">
-                            @foreach($parametro_select as $key_selected => $value_selected)
-                            @if($linha2->tipo == $key_selected)
-                            <div class="conteudo">
-                                <div class='produto '>
-                                    <div class="img ">
-
-                                        @if(asset($linha2->url_img))
-                                        <img src="{{asset($linha2->url_img)}}" id="imgPhoto" alt="" class="imgPhoto">
-                                        @else
-                                        <i class="bi bi-file-image-fill " style="font-size:50px"></i>
-                                        @endif
-                                    </div>
-                                    <div class="informacoes text-light">
-                                        <div class="nome">
-                                            <p>{{$linha2->nome_descritivo}}</p>
-                                        </div>
-
-                                        <div class="desc d-flex flex-column">
-                                            <p class='descricao '>{{$linha2->descricao}}</p>
-                                        </div>
-
-                                        <div class='valor d-flex flex-column'>
-                                            <p>R${{number_format($linha2->valor, 2, ',', '.')}}</p>
-
-                                        </div>
-                                    </div>
-                                </div>
-                                <hr class='sep'>
-                                <div class='acoes'>
-                                    <div class="editar">
-                                        <form method="GET" action="/produto/{{$linha2->id}}/edit">
-                                            @csrf
-                                            <input type="hidden" name="tokid" value="{{$tokid}}" />
-                                            <button type="submit" id="editarBotao" class="btn btn-second">
-                                                Alterar
-                                            </button>
-                                        </form>
-                                    </div>
-                                    <div class="apagar">
-                                        <form method="POST" action="/produto/{{$linha2->id}}">
-                                            @csrf
-                                            <input type="hidden" name="_method" value="DELETE" />
-                                            <button type="submit" id="excluirBotao" class="btn btn-primary">Excluir</button>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                            @endif
-                            @endforeach
                         </div>
-
-                        @endif
-
-                        @endforeach
-
-                    </div>
+                        <div class="line-two">
+                            <div class="form-group content-produto col-5">
+                                <label for="nome_descritivo">Nome do Produto </label>
+                                <input type="text" name="nome_descritivo" class="form-control" value="{{$produto->nome_descritivo}}" required />
+                            </div>
+                            <div class="form-group content-tipo col-4">
+                                <label for="tipo">Tipo de produto</label>
+                                <select name="tipo" class="form-control" class="inptselect form-control" required>
+                                    @foreach($parametro_select as $key_selected => $value_selected)
+                                    <option value="{{$key_selected}}">{{$value_selected}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group content-preco col-2">
+                                <label for="valor">Preço</label>
+                                <div id='preco' class="input-group ">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text" id="basic-addon1">R$</span>
+                                    </div>
+                                    <input type="text" id="valor" maxlength="8" name="valor" class="form-control" value="@if($produto->valor != 0)
+                                        {{number_format($produto->valor, 2, ',', '.')}}
+                                        @endif" />
+                                </div>
+                                <input type="hidden" id="valor_without_mask" name="valor_without_mask" value="{{ $produto->valor }}" />
+                            </div>
+                        </div>
+                        <div class="form-group col-12 descricao-form">
+                            <label for="descricao">Descriçao</label>
+                            <textarea class="textarea form-control" rows="2" name="descricao">{{$produto->descricao}}</textarea>
+                        </div>
+                        <div class="button-form row">
+                            <div class="col-2">
+                                <a href='/produto' type="button" class="btn btn-second bottom "><i class="fas fa-save"></i>Cancelar</a>
+                            </div>
+                            <div class="col-2">
+                                <button type="submit" class="btn btn-primary bottom"><i class="fas fa-save"></i>Salvar</button>
+                            </div>
+                        </div>
+                        <input type="hidden" name="id" value="{{$produto->id}}">
+                        <input type="hidden" name="tokid" value="{{$tokid}}">
+                    </form>
                 </div>
-                @endif
-                @endforeach
+            </main>
+        </div>
+        <hr>
+        <div class="produtos-genericos ">
+            <div class="content">
+                <!--contagem-->
+                <div class="count">
+                    <h2>{{$secao[2]}}</h2>
+                    <p class='lead'>total de produtos cadastrados: {{count($produtos)}}</p>
+                </div>
+                <!-- listagem -->
+                <div class="listagem">
+                    @php
+                    $conferir=[];
+                    @endphp
+                    @foreach($produtos as $linha)
+                    @if(in_array($linha->tipo, $conferir) == true)
+                    @else
+                    <hr>
+                    <div class='item'>
+                        <div class="tipo d-flex flex-column">
+                            <h1>
+                                @foreach($parametro_select as $key_selected => $value_selected)
+                                @if($linha->tipo == $key_selected)
+                                {{$value_selected}}
+                                {{$conferir[]=$linha->tipo}}
+                                @endif
+                                @endforeach
+                            </h1>
+                        </div>
+                        <div class="d-flex flex-row flex-wrap aqui">
+                            @foreach($produtos as $linha2)
+                            @if($linha->tipo==$linha2->tipo)
+                            <div class="cont me-5 mb-5 shadow">
+                                @foreach($parametro_select as $key_selected => $value_selected)
+                                @if($linha2->tipo == $key_selected)
+                                <div class="conteudo">
+                                    <div class='produto '>
+                                        <div class="img ">
+                                            @if(asset($linha2->url_img))
+                                            <img src="{{asset($linha2->url_img)}}" id="imgPhoto" alt="" class="imgPhoto">
+                                            @else
+                                            <i class="bi bi-file-image-fill " style="font-size:50px"></i>
+                                            @endif
+                                        </div>
+                                        <div class="informacoes text-light">
+                                            <div class="nome">
+                                                <p>{{$linha2->nome_descritivo}}</p>
+                                            </div>
+                                            <div class="desc d-flex flex-column">
+                                                <p class='descricao '>{{$linha2->descricao}}</p>
+                                            </div>
+                                            <div class='value d-flex flex-column'>
+                                                <span><strong>R${{number_format($linha2->valor, 2, ',', '.')}}</strong></span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <hr class='sep'>
+                                    <div class='acoes'>
+                                        <div class="editar">
+                                            <form method="GET" action="/produto/{{$linha2->id}}/edit">
+                                                @csrf
+                                                <input type="hidden" name="tokid" value="{{$tokid}}" />
+                                                <button type="submit" id="editarBotao" class="btn btn-second">
+                                                    Alterar
+                                                </button>
+                                            </form>
+                                        </div>
+                                        <div class="apagar">
+                                            <form method="POST" action="/produto/{{$linha2->id}}">
+                                                @csrf
+                                                <input type="hidden" name="_method" value="DELETE" />
+                                                <button type="submit" id="excluirBotao" class="btn btn-primary">Excluir</button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                                @endif
+                                @endforeach
+                            </div>
+                            @endif
+                            @endforeach
+                        </div>
+                    </div>
+                    @endif
+                    @endforeach
+                </div>
             </div>
         </div>
+        <div id='up'>
+            <button id="upPage">
+                <i class="bi bi-arrow-up-circle-fill"></i>
+            </button>
+        </div>
     </div>
-
-    <div id='up'>
-        <button id="upPage">
-            <i class="bi bi-arrow-up-circle-fill"></i>
-        </button>
-    </div>
+</body>
 
 </html>
 
